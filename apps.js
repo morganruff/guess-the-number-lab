435c3a948822bc0e70cffaa2b20f5d2acb2bf2a6
morganruff <morgan.ruff@duke.edu>
2021-02-25 16:31:31 -0500

    reset

--- apps.js
+++ apps.js
@@ -1,22 +1,59 @@
 const game = {
-    title: 'Guess the Number!',
-    biggestNum: 100,
-    smallestNum: 1,
-    secretNum: null,
-    prevGuesses: [],
-    play: function() {
-      this.secretNum = Math.floor(Math.random() * 
-        (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
-        return this.secretNum
-    },
-    guess: null,
-    message: prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`),
-    getGuess: function(){
-      return(this.message);
-      
-  },
+  title: 'Guess the Number!',
+  biggestNum: 100,
+  smallestNum: 1,
+  secretNum: null,
+  prevGuesses: [],
+  play: function() {
+    this.secretNum = Math.floor(Math.random() * 
+      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+  }
 }
 
-  console.log(game)
-  console.log(game.getGuess)
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const game = {
+//     title: 'Guess the Number!',
+//     biggestNum: 100,
+//     smallestNum: 1,
+//     secretNum: null,
+//     prevGuesses: [],
+//     play: function() {
+//       this.secretNum = Math.floor(Math.random() * 
+//         (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
+//         return this.secretNum
+//     },
+    
+    
+//     message: prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`),
+//     getGuess: function(){
+//       return(this.message);
+//       let guess = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`);
+//       if (guess > this.smallestNum && guess < this.greatestNum) {
+//         guess = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`)
+//       } 
+//       if (isNaN(guess) && guess > this.smallestNum && guess < this.greatestNum) {
+//         guess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`));
+//       } 
+//       if (guess < this.smallestNum || guess > this.greatestNum) {
+//         guess = null;
+//       }
+//       return guess
+//     }
+
+// }
